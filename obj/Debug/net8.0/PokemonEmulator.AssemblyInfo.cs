d9d0fa7118af5da1ef733539d3a29ea971cf18f9
Mads Ludvigsen <mlu@interflora.dk>
2024-07-25 19:04:10 +0200

    tryed the battle with trains

--- obj/Debug/net8.0/PokemonEmulator.AssemblyInfo.cs
+++ obj/Debug/net8.0/PokemonEmulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonEmulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66145adeae5bef1e6f9e36fd83642a8ceca6b1bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonEmulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonEmulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
